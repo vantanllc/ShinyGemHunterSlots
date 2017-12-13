08659936ded447d3ec75a962dff93d2df7edd579
Vantan LLC <vantan.llc@gmail.com>
2017-12-12 21:51:50 -0600

    Pod update

--- travis/script.sh
+++ travis/script.sh
@@ -3,6 +3,7 @@ if [ $TRAVIS_BRANCH = "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
   fastlane ios upload_metadata
   fastlane ios beta
 else
+  pod repo update
   open -b com.apple.iphonesimulator
   fastlane ios test || exit 10
   fastlane ios report_test_coverage
