09fa51f8a55b4bc3f1c89f9c575556a88eea7ab0
Vantan LLC <vantan.llc@gmail.com>
2017-12-12 22:04:50 -0600

    disable cocoa pods

--- travis/script.sh
+++ travis/script.sh
@@ -3,7 +3,6 @@ if [ $TRAVIS_BRANCH = "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
   fastlane ios upload_metadata
   fastlane ios beta
 else
-  pod repo update
   open -b com.apple.iphonesimulator
   fastlane ios test || exit 10
   fastlane ios report_test_coverage
